7b0fde51bfe74bb090bdda5dac103034742aae20
Denis Corol <denis@cargurus.com>
2016-09-29 19:08:44 -0400

    filter by a datacenter

--- lib/chef/knife/softlayer_server_list.rb
+++ lib/chef/knife/softlayer_server_list.rb
@@ -10,16 +10,20 @@ class Chef
 
       banner 'knife softlayer server list (options)'
 
+      option :datacenter,
+             :long => '--datacenter VALUE',
+             :description => 'Filter by a particular datacenter.',
+             :default => Chef::Config[:knife][:softlayer_default_datacenter]
       ##
       # Run the procedure to list all of the Softlayer VM's
       # @return [nil]
       def run
 
-        $stdout.sync = true
-        fmt = "%-20s %-8s %-15s %-15s %-10s"
-        puts ui.color(sprintf(fmt, "Name", "Location", "Public IP", "Private IP", "Status"), :green)
+        $stdout.sync = false
+        fmt = "%-8s %-30s %-8s %-15s %-15s %-10s"
+        puts ui.color(sprintf(fmt, "ID", "FQDN", "Location", "Public IP", "Private IP", "Status"), :green)
         connection.servers.each do |server|
-          puts sprintf fmt, server.name, server.datacenter, server.public_ip_address, server.private_ip_address, server.created_at ? 'Running' : 'Starting'
+          puts sprintf fmt, server.id, server.fqdn , server.datacenter, server.public_ip_address, server.private_ip_address, server.state if config[:datacenter].nil? || config[:datacenter] == server.datacenter
         end
       end
     end
