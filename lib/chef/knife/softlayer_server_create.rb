a4d16c12dbffc56a84b9f930bff6580243c974a9
Matt Eldridge <matt.eldridge@us.ibm.com>
2016-10-06 11:07:04 +0200

    Change network interface options hash key from symbol to string for updated compatibility with fog-softlayer and bump version to 0.4.5

--- lib/chef/knife/softlayer_server_create.rb
+++ lib/chef/knife/softlayer_server_create.rb
@@ -302,7 +302,7 @@ class Chef
         end
 
         # FIXME: make the above deal with nested opts and get rid of this one-off
-        opts[:network_components] = [ {:speed => config[:nic]} ] if !!config[:nic]
+        opts[:network_components] = [ {"speed" => config[:nic]} ] if !!config[:nic]
 
         opts.delete_if { |k,v| v.nil? }
         puts ui.color("Launching SoftLayer VM, this may take a few minutes.", :green)
